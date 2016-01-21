4a6430d14964b5107cb0bf4ba967f287ad14b292
Cara <hubbell.cara@gmail.com>
2016-01-21 08:44:57 -0700

    Plot3 script and resulting graph

---
+++ plot3.R
@@ -0,0 +1,26 @@
+## ---------------------------------------------------------------------------------
+## Exploratory Data Analysis
+## Week 1 Project
+## Cara Hubbell
+## January 2016
+## ---------------------------------------------------------------------------------
+##
+## This script creates Plot3. Note that you must run acquiredata.R first!
+
+# Read the data into R
+power_consumption<-read.table("./data/power_consumption.txt", header=TRUE, sep="|")
+
+#Create a variable that holds the date and time of each observation
+datetime<-paste(power_consumption$date,power_consumption$time,sep="")
+datetime<-strptime(datetime,format = "%Y-%m-%d %H:%M:%S")
+
+#Create the plot in a png file
+png("plot3.png", width=480, height=480, units="px")
+plot(datetime,power_consumption$submetering1, type="n", xlab="", 
+     ylab="Energy sub metering")
+points(datetime,power_consumption$submetering1, type="l")
+points(datetime,power_consumption$submetering2, type="l", col="red")
+points(datetime,power_consumption$submetering3, type="l", col="blue")
+legend("topright", lwd=1, lty=1, col=c("black","red","blue"), 
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
